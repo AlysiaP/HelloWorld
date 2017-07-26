2b434ec6ba28cf7b16b933d120450e4649d3d4cc
Alysia Petti <alysiapetti@gmail.com>
2017-07-26 15:18:25 -0500

    Add files via upload

---
+++ Hello_Ashford (1).java
@@ -0,0 +1,19 @@
+
+public class Hello_Ashford {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String name = "Alysia Petti";
+double Score1 = 80;
+double Score2 = 90;
+double Score3 = 100;
+double Average = (Score1 + Score2+ Score3)/3;
+//ready to print
+System.out.println(name);
+System.out.println(Score1);
+System.out.println(Score2);
+System.out.println(Score3);
+System.out.println(Average);
+	}
+
+}
